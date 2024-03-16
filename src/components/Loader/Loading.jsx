34f92a36cbbed88350367ea4090cce26b8bfe170
julio cesar conrado sisnero <conradojuliosisnero@gmail.com>
2024-03-16 09:53:49 -0500

    obtención datos correctamente de TMDB en la ruta /movies

---
+++ src/components/Loader/Loading.jsx
@@ -0,0 +1,14 @@
+import "./loader.scss";
+
+export default function Loading() {
+  return (
+    <div class="wrapper">
+      <div class="circle"></div>
+      <div class="circle"></div>
+      <div class="circle"></div>
+      <div class="shadow"></div>
+      <div class="shadow"></div>
+      <div class="shadow"></div>
+    </div>
+  );
+}
--- src/services/TMDB/GetMovies.js
+++ src/services/TMDB/GetMovies.js
@@ -1,11 +1,12 @@
-const getMovies = async (setMovieData) => {
+const getMovies = async (setMovieData, nextPage) => {
+  console.log(nextPage);
   try {
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=1`
+      `https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${nextPage}`
     );
     if (response.status === 200) {
-      const { results } = await response.json();
-      setMovieData(results);
+      const data = await response.json();
+      setMovieData(data);
     } else {
       console.error("algo salio mal");
     }
