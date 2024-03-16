08da6bbb81a0bd7d64df30caec09dc8d0a75beda
julio cesar conrado sisnero <conradojuliosisnero@gmail.com>
2024-03-15 19:48:53 -0500

    obtencion de peliculas en la ruta /movies

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const path = require("path");
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/sass")],
+    prependData: `@import "main.scss"`,
+  },
+};
+
+module.export = nextConfig;
