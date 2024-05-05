36749e5bc3d02de038652da3350d22ec2dd791f0
Conrado julio <conradojuliosisnero@gmail.com>
2024-05-05 18:20:25 -0500

    Actualizar data.js

--- src/components/Nav/data.js
+++ src/components/Nav/data.js
@@ -1,5 +1,5 @@
 export const optionMenu = [
-  //{ id: 1, name: "Home", path: "/" },
+  { id: 1, name: "Home", path: "/" },
   { id: 2, name: "Movies", path: "/movies-series/movies-section" },
   { id: 3, name: "Series", path: "/movies-series/series-section" },
   // { id: 4,name: "Help", path: "/about" },
