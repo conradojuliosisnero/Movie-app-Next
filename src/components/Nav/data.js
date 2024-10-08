5000b5147086e5c4279020162dbf931356f3a901
Conrado julio <conradojuliosisnero@gmail.com>
2024-10-08 10:35:08 -0500

    seccion de peliculas en cinees

--- src/components/Nav/data.js
+++ src/components/Nav/data.js
@@ -2,10 +2,12 @@ import Home from "../../../public/home.svg";
 import movie from "../../../public/movie.svg";
 import serie from "../../../public/serie.svg";
 import help from "../../../public/help.svg";
+import cine from "../../../public/cinema-icon.svg";
 
 export const optionMenu = [
   { id: 1, name: "Inicio", icon: Home, path: "/home" },
-  {id: 2,name: "Peliculas",icon: movie,path: "/movies"},
+  {id: 2, name: "Peliculas", icon: movie, path: "/movies"},
+  { id: 2, name: "Cines", icon: cine, path: "/en-cines" },
   { id: 3, name: "Series", icon: serie, path: "/series" },
   { id: 4, name: "Acerca", icon: help, path: "/about" },
 ];
